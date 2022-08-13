eb79b44e81b12af28bdd790d2200a86d960e76e0
David Smellie <dsmellie@umich.edu>
2022-08-12 21:46:30 -0500

    AddingPyPoll.py file

---
+++ Python_practice.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 12 17:18:22 2022
+
+@author: dsmel
+"""
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
